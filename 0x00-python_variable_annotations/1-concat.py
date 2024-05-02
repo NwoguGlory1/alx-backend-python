6dcdf4b9947e4d73b070eb77ffb3462db53ed255
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-02 14:01:40 +0000

    asic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings using f-string.
+
+    Parameters:
+    str1: The first string.
+    str2: The second string.
+
+    Returns:
+    A concatenated string: str1str2
+    """
+    return f"{str1}{str2}"
