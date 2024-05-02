172e51e35d56e31eb09611cf1816d4bbad57ed36
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-02 15:07:19 +0000

    Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This script demonstrates the use of type annotations in Python to specify
+the expected type of variables.
+"""
+
+    a: int = 1
+    pi: float = 3.14
+    i_understand_annotations: bool = True
+    school: str = "Holberton"
