856e7c20f46732f8289b9ee3434f864414e37599
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-02 17:10:41 +0000

    Complex types - functions

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+This script demonstrates the use of Union, Tuple type hint from
+the typing module to sum variables of different types.
+"""
+from typing import Callable
+""" imports Callable """
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Creates a function that multiplies a float by a given multiplier.
+
+    Parameters:
+    multiplier (float): The multiplier to be used in the multiplication.
+
+    Returns:
+    Callable[[float], float]: A function that takes a float and
+    returns the product of the float and the multiplier.
+
+    """
+    def inner_function(value: float) -> float:
+        return value * multiplier
+    return inner_function
