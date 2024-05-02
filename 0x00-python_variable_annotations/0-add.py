2e773870eb109a324893a5b9bcc2214886d17e4d
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-02 13:45:14 +0000

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def add(a: float, b: float) -> float:
+    """
+    Adds a and b as arguments.
+
+    Parameters:
+    a, b (float)
+
+    Returns:
+    the sum of a, b
+    """
+    total_sum = (a + b)
+    return total_sum
