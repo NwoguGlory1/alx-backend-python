eb823971465516f267bbbb8259dc9b8ae4dd9974
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-02 16:25:08 +0000

    Complex types - mixed list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+This script demonstrates the use of Union type hint from 
+the typing module to sum variables of different types.
+"""
+from typing import List, Union
+""" imports List """
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    The function uses sum built-in function to add
+    all the integers and floats in the input list.
+    There is no need to iterate over the list manually
+
+    Parameters:
+    mxd_lst (List[Union[int, float]]): A list containing integers and/or floats.
+
+    Returns:
+    float: The sum of all integers and floats in the input list.
+    """
+    return sum(mxd_lst)
