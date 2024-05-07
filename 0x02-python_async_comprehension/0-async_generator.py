2f551c6725c521d889b81a811a82bfc4293c94df
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-07 16:33:12 +0000

    Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 """ A script about async generator """
+
 import asyncio
 import random
 """ imports these modules """
 
 
 async def async_generator():
-    """ An async generator that takes no argument """
-    for i in range(0, 11):
+    """
+    An async generator that takes no argument,
+    yields random numbers between 0 and 10
+    after an asynchronous wait of 1 second """
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.randint(0, 10)
