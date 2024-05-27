5df1c7fe2c8c0dc7c5005f9bdaaffa332bbb1da7
NwoguGlory1 <glorynwogu18@gmail.com>
2024-05-27 15:14:49 +0000

    Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ Test """
 import unittest
+import requests
 from parameterized import parameterized
 from utils import access_nested_map
+from unittest.mock import patch
 """ import the necessary modules"""
 
 
@@ -31,3 +33,24 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map is called to see if exception is raised
             if it is raised, then access_nested_map is OK
             """
+
+
+class TestGetJson(unittest.TestCase):
+    """ Test class for get_json """
+
+    @parameterized.expand([
+        ('http://example.com', {"payload": True}),
+        ('http://holberton.io', {"payload": False}),
+        ])
+    @patch('requests.get')
+    def test_get_json(self, mock_get, test_url, test_payload):
+        """ method to test that utils.get_json returns expected result"""
+        mock_response = Mock()
+        """ inside the test, a Mock object is created """
+        mock_response.json.return_value = test_payload
+        """ json response of the mock object returns test_payload """
+        mock_get.return_value = mock_response
+
+        result = get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
