36202da4e9c66f11f002a0cd6bb2f1b64ff90696
NwoguGlory1 <glorynwogu18@gmail.com>
2024-06-01 04:15:46 +0000

    Parametrize and patch as decorators

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,8 +4,6 @@ import unittest
 from parameterized import parameterized
 from unittest.mock import patch, Mock
 from client import GithubOrgClient
-""" import the necessary modules"""
-
 
 class TestGithubOrgClient(unittest.TestCase):
     """Test class for GithubOrgClient"""
@@ -15,26 +13,25 @@ class TestGithubOrgClient(unittest.TestCase):
         ('abc',),
     ])
     @patch('client.get_json')
-    def test_org(self, org_name, mock_get_json):
+    def test_org(self, input, mock_get_json):
         """Method to test that GithubOrgClient.org returns the correct value"""
-
+        
         # Define the mock return value
-        mock_response = {"login": org_name}
+        mock_response = {"login": input}
         mock_get_json.return_value = mock_response
 
         # Instantiate the GithubOrgClient with the test organization
-        client = GithubOrgClient(org_name)
+        test_class = GithubOrgClient(input)
 
         # Call the org method
-        result = client.org()
+        result = test_class.org()
 
         # Verify that get_json was called once with the expected URL
-        expected_url = f"https://api.github.com/orgs/{org_name}"
+        expected_url = f"https://api.github.com/orgs/{input}"
         mock_get_json.assert_called_once_with(expected_url)
 
         # Verify the result of the org method
         self.assertEqual(result, mock_response)
-
 
 if __name__ == '__main__':
     unittest.main()
